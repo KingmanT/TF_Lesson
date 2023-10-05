b94cc0742c850fe589cee296a525943e8a790375
Kingman <107763929+KingmanT@users.noreply.github.com>
2023-10-05 17:41:25 -0400

    Create main.tf

---
+++ ec2_manualVPC/main.tf
@@ -0,0 +1,65 @@
+# configure aws provider
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  region = "us-east-1"
+  #profile = "Admin"
+}
+
+# create instance
+resource "aws_instance" "web_server01" {
+  ami = "ami-08c40ec9ead489470"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-044c5701db0847a45"
+  security_groups = [aws_security_group.web_ssh.id]
+  #vpc_security_group_ids = [aws_security_group.web_ssh.id]
+  #key_name = "ubuntuSandbox"
+
+  #user_data = "${file("deploy.sh")}"
+
+  tags = {
+    "Name" : "tf_made_instance"
+  }
+
+}
+
+# create security groups
+
+resource "aws_security_group" "web_ssh" {
+  name        = "tf_made_sg"
+  description = "open ssh traffic"
+  vpc_id = "vpc-000fefecf8fefb19c"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" : "tf_made_sg"
+    "Terraform" : "true"
+  }
+
+}
+
+output "instance_ip" {
+  value = aws_instance.web_server01.public_ip
+}
