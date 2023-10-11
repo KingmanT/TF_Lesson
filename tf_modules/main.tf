07372f1cac59ba7077116f46e0c47342814d4068
Kingman <107763929+KingmanT@users.noreply.github.com>
2023-10-11 16:41:35 -0400

    Update main.tf

--- tf_modules/main.tf
+++ tf_modules/main.tf
@@ -4,31 +4,25 @@ variable "aws_secret_key" {}
 # configure aws provider
 provider "aws" {
   region = var.region
-  #profile = "Admin"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
-# create vpc
+# Create VPC
 module "vpc" {
-  source       = "../vpc"
-  region       = var.region
+  source = "./vpc"
+  region = var.region
   project_name = var.project_name
-  vpc_cidr     = var.vpc_cidr
-
 }
 
-#create instance
+# Create Instance
 module "instance" {
-  source              = "../instance"
-  ami                 = var.ami
-  instance_type       = var.instance_type
-  instance_name       = var.instance_name
+  source = "./instance"
+  project_name = var.project_name
+  ami = var.ami
+  instance_type = var.instance_type
   key_name            = var.key_name
-  security_group_name = var.security_group_name
   subnet_id           = module.vpc.subnet_id
   vpc_id              = module.vpc.vpc_id
-}
 
-
-output "instance_ip" {
-  value = aws_instance.web_server01.public_ip
 }
